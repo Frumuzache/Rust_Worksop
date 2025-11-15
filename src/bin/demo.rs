e592d552c4a6a21a93e5d6ec9f33d7163ce32529
Frumuzache <frumuzache.alexandru4@gmail.com>
2025-11-15 17:04:02 +0200

    added servo stuff Adi

--- src/bin/demo.rs
+++ src/bin/demo.rs
@@ -1,82 +1,105 @@
 #![no_std]
 #![no_main]
 
-
 use defmt::info;
-use embassy_executor::Spawner;
-use embassy_stm32::{self as _, Config};
 use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::OutputType;
+use embassy_stm32::peripherals::TIM1;
+use embassy_stm32::peripherals::TIM2;
+use embassy_stm32::peripherals::TIM3;
+use embassy_stm32::time::khz;
+use embassy_stm32::timer::Ch1;
+
+use embassy_stm32::timer::Ch2;
+use embassy_stm32::timer::Ch3;
+use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
+use embassy_stm32::{self as _, Config};
 use embassy_time::Timer;
 use panic_probe as _;
-use embassy_stm32::time::khz;
-use embedded_workshop_skeleton::music::{Note, OCTAVE};
 
 
-
-/// Beats per minute.
-const TEMPO: u64 = 100;
-/// A whole note duration in milliseconds.
-const WHOLE_NOTE: u64 = 4 * (60_000 / TEMPO);
-/// The microcontroller clock frequency
-const CLOCK_FREQ: u64 = 150_000_000;
-/// PWM clock divider
-const PWM_DIV: u64 = 64;
-
-
-use embassy_stm32::time::hz;
-use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
-use embassy_stm32::peripherals::TIM2;
-use embassy_stm32::timer::{Ch1, Ch2};
-use embassy_stm32::gpio::OutputType;
-
-// The original song is about 114 BPM
-// You might want to change your TEMPO constant:
-// const TEMPO: u64 = 114;
+use embassy_stm32::bind_interrupts;
+use embassy_stm32::i2c::{self, I2c};
 
 
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    
     let config = Config::default();
-    let p = embassy_stm32::init(config);
-
-    let buzzer: PwmPin<'_, TIM2, Ch2> = PwmPin::new(p.PB3, OutputType::PushPull);
-
-
+    let p = embassy_stm32::init(Default::default());
+    let led_pwm_pin: PwmPin<'_, TIM2, Ch2> = PwmPin::new(p.PB3, OutputType::PushPull);
     let mut pwm = SimplePwm::new(
-    p.TIM2,              // Timer 2 peripheral
-    None,   
-    Some(buzzer),                // Channel 2 not used
-    None,                // Channel 3 not used
-    None,                // Channel 4 not used
-    khz(1),              // PWM frequency = 1 kHz
-    Default::default(),  // Default configuration
+        p.TIM2,
+        None,
+        Some(led_pwm_pin),
+        None,
+        None,
+        khz(1),
+        Default::default(),
+    );
+
+
+    let led2_pwm_pin: PwmPin<'_, TIM3, Ch1> = PwmPin::new(p.PB4, OutputType::PushPull);
+    let mut pwm2 = SimplePwm::new(
+        p.TIM3,
+        Some(led2_pwm_pin),
+        None,
+        None,
+        None,
+        khz(1),
+        Default::default(),
     );
 
 
 
-    info!("Playing melody...");
-    pwm.ch2().enable();
+
+    let mut ch2 = pwm.ch2(); // Get handle for channel 2
+    ch2.enable();
+    let mut ch1 = pwm2.ch1(); // Get handle for channel 1
+    ch1.enable();
     
+    ch2.set_duty_cycle_percent(99);
+    ch1.set_duty_cycle_percent(99);
+
+
+
+    let mut bmp = Bmp280::new_async(i2c, Delay);
     
-loop {
-    for (note, duration) in SONG.iter() {
-        
-        if let Some(n) = note {
-            pwm.set_frequency(hz((*n as u32)));
-            pwm.ch2().set_duty_cycle_percent(50);
-        } 
-        
-        else {
-            pwm.ch2().set_duty_cycle(0); // Silence
-        }
-        
-        let note_duration = WHOLE_NOTE / (*duration as u64);
-        Timer::after(embassy_time::Duration::from_millis(note_duration)).await;
+    if let Err(e) = bmp.init().await {
+        defmt::error!("Failed to initialize BMP280: {:?}", e);
+        loop {}
     }
-}
+
+
+    
+    loop {
+        for i in 99..2 {
+            ch2.set_duty_cycle_percent(i);
+            Timer::after_millis(100).await;
+        }
+        for i in 2..99 {
+            ch2.set_duty_cycle_percent(i);
+            Timer::after_millis(100).await;
+        }
+
+        for i in 99..2 {
+            ch1.set_duty_cycle_percent(i);
+            Timer::after_millis(100).await;
+        }
+        for i in 2..99 {
+            ch1.set_duty_cycle_percent(i);
+            Timer::after_millis(100).await;
+        }
 
 
 
+
+        /*  Timer::after_secs(1).await;
+        ch2.set_duty_cycle_percent(2);
+        ch1.set_duty_cycle_percent(99);
+        Timer::after_secs(1).await;
+        ch2.set_duty_cycle_percent(99);
+        ch1.set_duty_cycle_percent(2);*/
+    }
 }
--- src/bin/wave.rs
+++ src/bin/wave.rs
@@ -1,21 +1,71 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
-use embassy_executor::Spawner;
-use embassy_stm32::{self as _, Config};
-
+use defmt::*;
 use defmt_rtt as _;
+use embassy_executor::Spawner;
 use embassy_time::Timer;
 use panic_probe as _;
 
+use embassy_stm32::{self as _, Config};
+
+// ADC
+use embassy_stm32::adc::{Adc, Resolution};
+
+// PWM
+use embassy_stm32::gpio::OutputType;
+use embassy_stm32::peripherals::{ADC1, TIM2};
+use embassy_stm32::time::hz;
+use embassy_stm32::timer::{Ch3, simple_pwm::PwmPin, simple_pwm::SimplePwm};
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let config = Config::default();
-    let _p = embassy_stm32::init(config);
+    let p = embassy_stm32::init(Config::default());
+    info!("Smile & Wave starting");
+
+    let mut adc = Adc::new(p.ADC1);
+    adc.set_resolution(Resolution::BITS12);
+    let max_adc: u32 = (1 << 12) - 1;
+
+    let mut joy_x = p.PA0;
+
+    let servo_pin: PwmPin<'_, TIM2, Ch3> = PwmPin::new(p.PB10, OutputType::PushPull);
+
+    let mut servo_pwm = SimplePwm::new(
+        p.TIM2,
+        None,
+        None,
+        Some(servo_pin),
+        None,
+        hz(50),
+        Default::default(),
+    );
+    let max_duty: u16 = servo_pwm.max_duty_cycle();
+
+    let mut servo_ch = servo_pwm.ch3();
+    servo_ch.enable();
+
+    // Servo pulse range
+    const SERVO_PERIOD_S: f32 = 1.0 / 50.0; // 20 ms
+    const SERVO_MIN_PW_S: f32 = 0.2e-3; // 1.0 ms
+    const SERVO_MAX_PW_S: f32 = 3.0e-3; // 2.0 ms
 
     loop {
-        info!("Hello");
-        Timer::after_secs(1).await;
+        // 1. Read joystick
+        let raw: u16 = adc.blocking_read(&mut joy_x);
+        let t = (raw as f32 / max_adc as f32).clamp(0.0, 1.0);
+
+        // 2. Map joystick -> pulse width
+        let pulse_s = SERVO_MIN_PW_S + t * (SERVO_MAX_PW_S - SERVO_MIN_PW_S);
+
+        // 3. Pulse width -> duty
+        let duty_f = (pulse_s / SERVO_PERIOD_S) * max_duty as f32;
+        let duty_u16: u16 = duty_f.clamp(0.0, max_duty as f32) as u16;
+
+        servo_ch.set_duty_cycle(duty_u16);
+
+        info!("adc={} t={:?} duty={}", raw, t, duty_u16);
+
+        Timer::after_millis(20).await;
     }
 }
