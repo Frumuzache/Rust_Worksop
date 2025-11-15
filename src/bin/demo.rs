b1415c1967542847a05be6f4379badf3b6144e1b
Frumuzache <frumuzache.alexandru4@gmail.com>
2025-11-15 14:20:42 +0200

    button example coe

--- src/bin/demo.rs
+++ src/bin/demo.rs
@@ -1,21 +1,40 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
+use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::{self as _, Config};
-
-use defmt_rtt as _;
+use embassy_stm32::gpio::Input;
+use embassy_stm32::gpio::Pull;
+use embassy_stm32::gpio::{Level, Output, Speed};
 use embassy_time::Timer;
-use panic_probe as _;
+use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let config = Config::default();
-    let _p = embassy_stm32::init(config);
+async fn main(_spawner: Spawner) -> ! {
+    let p = embassy_stm32::init(Default::default());
+    let buttonS1 = Input::new(p.PB4, Pull::Up);
+    let buttonS2 = Input::new(p.PB5, Pull::Up);
+    let buttonS3 = Input::new(p.PB3, Pull::Up);
+    let buttonS4 = Input::new(p.PC8, Pull::Up);
 
     loop {
-        info!("Hello");
-        Timer::after_secs(1).await;
+        if buttonS1.is_low() {
+            defmt::info!("SQUAD CAL RETARDED");
+        }
+
+        if buttonS2.is_low() {
+            defmt::info!("SQUAD CAL RETARDED LMAO");
+        }
+
+        if buttonS3.is_low() {
+            defmt::info!("SQUAD CAL RETARDED BRUH");
+        }
+
+        if buttonS4.is_low() {
+            defmt::info!("SQUAD CAL RETARDED YOOOO");
+        }
+
+        // Wait for 100 milliseconds before checking again
+        Timer::after(embassy_time::Duration::from_millis(100)).await;
     }
 }
